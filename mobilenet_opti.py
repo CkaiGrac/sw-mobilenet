8d9f02e69a03c4eec6c7f6ce4670bf071fab21ce
CkaiGrac <1043932993@qq.com>
2019-12-15 14:25:34 +0800

    修改网络结构

--- mobilenet_opti.py
+++ mobilenet_opti.py
@@ -35,11 +35,11 @@ class Mobilenet:
                                                   moving_variance_initializer=tf.ones_initializer(), training=self.trainable,
                                                   name='dw/bn')
             # relu = tf.nn.leaky_relu(bn_dw, 0.1)
-            h_swish = self.hard_swish(bn_dw)
+            # h_swish = self.hard_swish(bn_dw)
             weight = tf.get_variable(name='weight', dtype=tf.float32, trainable=True,
                                      shape=(1, 1, dw_filter[2]*dw_filter[3], output_channel), initializer=tf.random_normal_initializer(stddev=0.01))
 
-            conv = tf.nn.conv2d(input=h_swish, filter=weight, strides=[
+            conv = tf.nn.conv2d(input=bn_dw, filter=weight, strides=[
                                 1, 1, 1, 1], padding="SAME", name="conv/s1")
             bn_pt = tf.layers.batch_normalization(conv, beta_initializer=tf.zeros_initializer(),
                                                   gamma_initializer=tf.ones_initializer(),
@@ -58,7 +58,7 @@ class Mobilenet:
                                      kernel_size=(3, 3),
                                      strides=(2, 2),
                                      padding='same',
-                                     activation=self.hard_swish,   # tf.nn.relu,
+                                     activation=tf.nn.relu6,   # tf.nn.relu,
                                      name='conv1'
                                      )
             bn1 = tf.layers.batch_normalization(conv1, beta_initializer=tf.zeros_initializer(),
@@ -81,7 +81,7 @@ class Mobilenet:
             x = self.separable_conv_block(input=x, dw_filter=(3, 3, 256, 1), output_channel=256,
                                           strides=(1, 1, 1, 1), name="spearable_5")
             route1 = x
-            #opti
+            # opti
             x = self.separable_conv_block(input=x, dw_filter=(3, 3, 256, 1), output_channel=512,
                                           strides=(1, 2, 2, 1), name="spearable_6")
 
@@ -103,8 +103,8 @@ class Mobilenet:
             x = self.separable_conv_block(input=x, dw_filter=(3, 3, 512, 1), output_channel=1024,
                                           strides=(1, 1, 1, 1), name="spearable_11")
 
-            # x = self.separable_conv_block(input=x, dw_filter=(3, 3, 1024, 1), output_channel=1024,
-            #                               strides=(1, 1, 1, 1), name="spearable_13")
+            x = self.separable_conv_block(input=x, dw_filter=(3, 3, 1024, 1), output_channel=1024,
+                                          strides=(1, 1, 1, 1), name="spearable_12")
         return route1, route2, x
 
 
