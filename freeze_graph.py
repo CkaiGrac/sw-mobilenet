bbec79a6a9d970a879694368039b7ce040816438
CkaiGrac <1043932993@qq.com>
2020-01-07 15:34:03 +0800

    bugfix freeze_graph

--- freeze_graph.py
+++ freeze_graph.py
@@ -6,24 +6,22 @@ from mobilenet import Mobilenet
 
 pb_file = "./mobilenet_normal.pb"
 ckpt_file = "./ckpt/mobileNet_test_acc=0.7056.ckpt"
-output_node_names = ["input/input_data", "transfer_learning/prediction", "transfer_learning/sqz"]
-
-with tf.name_scope('input'):
-    input_data = tf.placeholder(dtype=tf.float32, shape=(None,224,224,3), name='input_data')
-
-sqz,predictions = Mobilenet(input_data,trainable=False)
-
-sess  = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-saver = tf.train.Saver()
-saver.restore(sess, ckpt_file)
-
-converted_graph_def = tf.graph_util.convert_variables_to_constants(sess,
-                            input_graph_def  = sess.graph.as_graph_def(),
-                            output_node_names = output_node_names)
-
-with tf.gfile.GFile(pb_file, "wb") as f:
-    f.write(converted_graph_def.SerializeToString())
+output_node_names = ["MobileNet/prediction"]
 
 
+saver = tf.train.import_meta_graph(
+    './ckpt/mobileNet_test_acc=0.7056.ckpt.meta', clear_devices=True)
+graph = tf.get_default_graph()
+input_graph_def = graph.as_graph_def()
 
+with tf.Session() as sess:
+    saver.restore(sess, ckpt_file)
+    output_graph_def = tf.graph_util.convert_variables_to_constants(  # 模型持久化，将变量值固定
+        sess=sess,
+        input_graph_def=input_graph_def,  # 等于:sess.graph_def
+        output_node_names=output_node_names)  # 如果有多个输出节点，以逗号隔开
 
+    with tf.gfile.GFile(pb_file, "wb") as f:  # 保存模型
+        f.write(output_graph_def.SerializeToString())  # 序列化输出
+    print("%d ops in the final graph." %
+          len(output_graph_def.node))  # 得到当前图有几个操作节点
---
+++ freeze_graphfake.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+import tensorflow as tf
+from mobilenet import Mobilenet
+
+pb_file = "./ckpt/mobilenet_normal.pb"
+ckpt_file = "./ckpt/mobileNet_test_acc=0.7056.ckpt"
+output_node_names = ["input/input_data", "transfer_learning/prediction", "transfer_learning/sqz"]
+
+with tf.name_scope('input'):
+    input_data = tf.placeholder(dtype=tf.float32, shape=(None,224,224,3), name='input_data')
+
+sqz,predictions = Mobilenet(input_data,trainable=False)
+
+sess  = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+saver = tf.train.Saver()
+saver.restore(sess, ckpt_file)
+
+converted_graph_def = tf.graph_util.convert_variables_to_constants(sess,
+                            input_graph_def  = sess.graph.as_graph_def(),
+                            output_node_names = output_node_names)
+
+with tf.gfile.GFile(pb_file, "wb") as f:
+    f.write(converted_graph_def.SerializeToString())
+
+
+
+
